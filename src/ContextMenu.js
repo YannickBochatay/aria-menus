67ab0bd11a889e8d2aa648424a403ab65745e0ab
Yannick Bochatay <yannick.bochatay@free.fr>
2026-04-06 08:43:08 +0200

    add context menu

--- src/ContextMenu.js
+++ src/ContextMenu.js
@@ -1,139 +1,84 @@
-import React, { Component } from "react"
-import ReactDOM from "react-dom"
-import PropTypes from "prop-types"
+const template = document.createElement("template");
+template.innerHTML = `
+  <style>
+  :host {
+    display:none;
+    position:fixed;
+  }
+  </style>
+  <slot></slot>
+`
 
-class ContextMenu extends Component {
+export default class ContextMenu extends HTMLElement {
 
-  constructor(props) {
-
-    super(props)
-
-    this.state = { display : false, position : { x : 0, y : 0 } }
-
-    this.handleBlurWindow = this.handleBlurWindow.bind(this)
-    this.handleClickDoc = this.handleClickDoc.bind(this)
-    this.handleContextMenu = this.handleContextMenu.bind(this)
-
-    this.menu = React.createRef()
+  constructor() {
+    super()
+    const root = this.attachShadow({ mode : "open" });
+    root.append(template.content.cloneNode(true));
   }
 
-  close() { this.setState({ display : false }) }
-
-  handleBlurWindow() { this.close() }
-
-  handleClickDoc(e) {
-
-    if (!this.menu.current) return
-
-    const { current } = this.menu
-
-    if (current && !current.contains(e.target)) this.close()
+  get target() {
+    return document.querySelector("#" + this.getAttribute("for"));
   }
 
-  handleContextMenu(e) {
-
-    e.preventDefault()
-    e.persist()
-
-    this.setState({ display : true }, () => this.setPosition(e))
-
-    if (this.props.onContextMenu) this.props.onContextMenu(e)
+  close() {
+    this.style.display = "none";
+    this.target.setAttribute("aria-expanded", false);
   }
 
-  setPosition(e) {
+  open(e) {
+    this.style.display = "inline-block";
+    const {x,y} = this.#computePosition(e);
+    this.style.left = x + "px";
+    this.style.top = y + "px";
+    this.target.setAttribute("aria-expanded", true);
+  }
 
-    if (!this.menu) return
+  #handleBlurWindow = () => this.close()
 
-    const { current } = this.menu
+  #handleClickDoc = e => {
+    if (!(this.contains(e.target))) this.close();
+  }
 
-    if (!current) return
+  #computePosition(e) {
 
-    let x = e.clientX
-    let y = e.clientY
+    const rect = this.getBoundingClientRect();
+    const clientX = e?.clientX ?? rect.left;
+    const clientY = e?.clientY ?? rect.top;
+    let x = clientX;
+    let y = clientY;
 
-    if (e.clientX + current.offsetWidth > window.innerWidth) {
-
-      x -= current.offsetWidth
-      if (x < 0) x = window.innerWidth - current.offsetWidth
-
+    if (clientX + rect.width > window.innerWidth) {
+      x -= rect.width
+      if (x < 0) x = window.innerWidth - rect.width
     }
 
-    if (e.clientY + current.offsetHeight > window.innerHeight) {
-
-      y -= current.offsetHeight
-      if (y < 0) y = window.innerHeight - current.offsetHeight
-
+    if (clientY + rect.height > window.innerHeight) {
+      y -= rect.height
+      if (y < 0) y = window.innerHeight - rect.height
     }
 
-    this.setState({ position : { x, y } })
-
+    return { x, y };
   }
 
-  addEventListeners() {
-    document.addEventListener("mousedown", this.handleClickDoc)
-    window.addEventListener("blur", this.handleBlurWindow)
+  #handleContextMenu = e => {
+    e.preventDefault();
+    this.open(e);
   }
 
-  removeEventListeners() {
-    document.removeEventListener("mousedown", this.handleClickDoc)
-    window.removeEventListener("blur", this.handleBlurWindow)
+  connectedCallback() {
+    document.addEventListener("mousedown", this.#handleClickDoc);
+    window.addEventListener("blur", this.#handleBlurWindow);
+    this.target.addEventListener("contextmenu", this.#handleContextMenu);
   }
 
-  componentDidUpdate(prevProps, prevState) {
-    if (this.state.display && !prevState.display) {
-      this.addEventListeners()
-    } else if (!this.state.display && prevState.display) {
-      this.removeEventListeners()
-    }
-  }
-
-  componentWillUnmount() {
-    this.removeEventListeners()
-  }
-
-  handlePreventDefault(e) { e.preventDefault() }
-
-  render() {
-
-    const { children, menu, ...rest } = this.props
-
-    const content = React.Children.only(children)
-
-    const container = React.cloneElement(content, {
-      key : "container",
-      ...rest,
-      onContextMenu : this.handleContextMenu
-    })
-
-    if (this.state.display) {
-
-      const contextMenu = ReactDOM.createPortal((
-        <div
-          key="contextMenu"
-          ref={ this.menu }
-          onContextMenu={ this.handlePreventDefault }
-          style={ {
-            position : "fixed",
-            left : this.state.position.x,
-            top : this.state.position.y
-          } }
-        >
-          { menu }
-        </div>
-      ), document.body)
-
-      return [container, contextMenu]
-
-    } else return container
-
+  disconnectedCallback() {
+    document.removeEventListener("mousedown", this.#handleClickDoc);
+    window.removeEventListener("blur", this.#handleBlurWindow);
+    this.target.removeEventListener("contextmenu", this.#handleContextMenu);
   }
 
 }
 
-ContextMenu.propTypes = {
-  children : PropTypes.node,
-  menu : PropTypes.node,
-  onContextMenu : PropTypes.func
-}
+customElements.define("desktop-contextmenu", ContextMenu);
 
-export default ContextMenu
