e1b7e7cf4158ae35897afe924c410bbbc8ca73f5
Yannick Bochatay <yannick.bochatay@free.fr>
2026-05-23 08:50:30 +0200

    first real test with playwright

--- tests/menu-list.spec.js
+++ tests/menu-list.spec.js
@@ -1,9 +1,15 @@
 // @ts-check
 import { test, expect } from '@playwright/test';
 
-test('has title', async ({ page }) => {
+test('keyboard navigation', async ({ page }) => {
   await page.goto('http://127.0.0.1:8080/tests/menu-list.html');
 
   // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle(/<menu-list> tests/);
+
+  await page.keyboard.press("Tab");
+
+  const item = page.getByRole("menuitem").first();
+
+  await expect(item).toBeFocused();
 });
