e412d051824e49933d9d11715005decfbcad0cb1
Yannick Bochatay <yannick.bochatay@free.fr>
2026-05-24 09:57:15 +0200

    Arrows navigation tests

--- tests/menu-list.spec.js
+++ tests/menu-list.spec.js
@@ -1,15 +1,44 @@
 // @ts-check
 import { test, expect } from '@playwright/test';
 
+
+
 test('keyboard navigation', async ({ page }) => {
   await page.goto('http://127.0.0.1:8080/tests/menu-list.html');
 
-  // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle(/<menu-list> tests/);
 
-  await page.keyboard.press("Tab");
+  const press = page.keyboard.press.bind(page.keyboard);
 
-  const item = page.getByRole("menuitem").first();
+  await press("Tab");
 
-  await expect(item).toBeFocused();
+  const items = page.getByRole("menuitem");
+
+  await expect(items).toHaveCount(6);
+
+  const firstItem = items.first();
+
+  await expect(firstItem).toBeFocused();
+
+  press("ArrowDown");
+  await expect(items.nth(1)).toBeFocused();
+
+  press("ArrowDown");
+  // third item is disabled
+  await expect(items.nth(3)).toBeFocused();
+
+  press("ArrowDown");
+  await expect(items.nth(4)).toBeFocused();
+
+  press("ArrowDown");
+  await expect(items.nth(5)).toBeFocused();
+
+  press("ArrowDown");
+  // back to first item
+  await expect(items.first()).toBeFocused();
+
+  press("ArrowUp");
+  // back to last item
+  await expect(items.last()).toBeFocused();
+
 });
